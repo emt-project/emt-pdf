7af643e51045522a9cd0ae173ece37d93addf44a
csae8092 <p.andorfer@gmail.com>
2026-02-12 08:46:59 +0100

    try: build all pdfs

--- make_pdf.sh
+++ make_pdf.sh
@@ -7,14 +7,9 @@ xelatex -interaction=nonstopmode tmp.tex
 xelatex -interaction=nonstopmode tmp.tex
 mv tmp.pdf ../html/emt.pdf
 
-counter=0
+
 for texfile in *.tex; do
     if [ "$texfile" != "tmp.tex" ]; then
-        ((counter++))
-        if [ $counter -gt 20 ]; then
-            echo "Reached 20 files, stopping..."
-            break
-        fi
         echo "Processing $texfile..."
         basename=$(basename "$texfile" .tex)
         xelatex -interaction=nonstopmode "$texfile"
