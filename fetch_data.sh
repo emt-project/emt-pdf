4edda6e2e9b4dd48a1f883e284f5819f078f45ab
csae8092 <p.andorfer@gmail.com>
2026-02-12 08:01:04 +0100

    feat: add current date to index.html

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+file="./html/index.html"
+today=$(date +%F)
+# Use sed to replace any existing content between the span tags
+sed -i -E "s!(<span id=\"update-span\">)[^<]*(</span>)!\1${today}\2!" "$file"
+echo "Updated $file with date $today"
 
